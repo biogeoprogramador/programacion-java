201104d1642069dedae01539be71c70e4d5f93d0
biogeoprogramador <152619245+biogeoprogramador@users.noreply.github.com>
2024-06-26 15:58:50 +0200

    Update Ejercicio_1.java

--- Ejercicios_Juan_Antonio_López_Quesada/Ejercicio_1.java
+++ Ejercicios_Juan_Antonio_López_Quesada/Ejercicio_1.java
@@ -4,33 +4,35 @@ import java.util.Scanner;
 
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        // variables
+        // crear objeto Scanner para el input por terminal
         Scanner input = new Scanner(System.in);
+        // variables
         int num, resto;
         boolean aux = true;
-        // controlar excepcion de input no numérico
+        // bucle while hasta que se complete la ejecución del programa
         while (aux) {
+            // control de excepción InputMismatchException
             try {
                 num = input.nextInt();
+                // comprobar que el número no sea 0
                 if (num == 0) {
                     System.out.println("Introduzca un número entero distinto a 0.");
                 } else {
+                    // comprobar si el número es par o impar
                     resto = num % 2;
-
                     if (resto == 0) {
-                        System.out.println(num + " es un número par.");
+                        System.out.println(num + " es un número PAR.");
                         aux = false;
                     } else {
-                        System.out.println(num + " es un número impar.");
+                        System.out.println(num + " es un número IMPAR.");
                         aux = false;
                     }
                 }
-
             } catch (Exception InputMismatchException) {
                 System.out.println("Introduzca un número entero distinto a 0.");
                 input.nextLine();
             }
         }
-
+        input.close();
     }
 }
