2f3e6b8bf5a038c78e8a2529dd584995fb49baaa
biogeoprogramador <152619245+biogeoprogramador@users.noreply.github.com>
2024-06-27 22:23:16 +0200

    Ejercicio 18

---
+++ Ejercicios_Juan_Antonio_López_Quesada/Ejercicio_18.java
@@ -0,0 +1,68 @@
+package Ejercicios_Juan_Antonio_López_Quesada;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Ejercicio_18 {
+    // Leer una secuencia de números y mostrar los 30 primeros pares leídos.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int limInf, limSup, count = 0, num;
+        System.out.println("Rango de números para mostrar los 30 primeros números pares.");
+        System.out.println("Introduzca el límite inferior del rango.");
+        limInf = numInput(input);
+        System.out.println("Introduzca el límite superior del rango.");
+        limSup = numInput(input);
+        System.out.println("--------------------");
+        for (int i = limInf; i <= limSup; i++) {
+            if (i % 2 == 0 && i != 0) {
+                System.out.println(i);
+                count++;
+            }
+            if (count == 30) {
+                break;
+            }
+        }
+        input.close();
+    }
+
+    public static int numInput(Scanner input) {
+        boolean aux = true;
+        int num = 0;
+        System.out.println("Introduzca un número entero");
+        while (aux) {
+            // control de excepción InputMismatchException
+            try {
+                // entrada de número
+                num = input.nextInt();
+                input.nextLine();
+                aux = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("Introduzca un número entero mayor que 0.");
+                input.nextLine();
+            }
+        }
+        return num;
+    }
+
+    public static int numInputPositive(Scanner input) {
+        boolean aux = true;
+        int num = 0;
+        System.out.println("Introduzca un número entero");
+        while (aux) {
+            try {
+                num = input.nextInt();
+                input.nextLine();
+                if (num > 0) {
+                    aux = false;
+                } else {
+                    System.out.println("Introduzca un número entero mayor que 0.");
+                }
+            } catch (InputMismatchException ex) {
+                System.out.println("Introduzca un número entero mayor que 0.");
+                input.nextLine();
+            }
+        }
+        return num;
+    }
+}
